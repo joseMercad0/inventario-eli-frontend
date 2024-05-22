1710c60711380ce897c631a1ac2167bc6a768a36
Jose Manuel Mercado Chuctaya <102069698+joseMercad0@users.noreply.github.com>
2024-05-22 14:53:44 -0500

    Update authSlice.js

--- src/redux/features/auth/authSlice.js
+++ src/redux/features/auth/authSlice.js
@@ -1,10 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const name = JSON.parse(localStorage.getItem("name"))
+const storedName = localStorage.getItem("name");
+let name;
+
+try {
+  name = storedName ? JSON.parse(storedName) : "";
+} catch (error) {
+  console.error("Error parsing stored name:", error);
+  name = "";
+}
 
 const initialState = {
     isLoggedIn: false,
-    name: name ? name : "",
+    name: name,
     user: {
       name: "",
       email: "",
@@ -12,21 +20,20 @@ const initialState = {
       bio: "",
       photo: "",
     },
-   
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    SET_LOGIN(state, action){
-      state.isLoggedIn = action.payload
+    SET_LOGIN(state, action) {
+      state.isLoggedIn = action.payload;
     },
-    SET_NAME(state, action){
-      localStorage.setItem("name", JSON.stringify(action.payload))
-      state.name = action.payload
+    SET_NAME(state, action) {
+      localStorage.setItem("name", JSON.stringify(action.payload));
+      state.name = action.payload;
     },
-    SET_USER(state, action){
+    SET_USER(state, action) {
       const profile = action.payload;
       state.user.name = profile.name;
       state.user.email = profile.email;
@@ -37,10 +44,10 @@ const authSlice = createSlice({
   },
 });
 
-export const {SET_LOGIN, SET_NAME, SET_USER} = authSlice.actions;
+export const { SET_LOGIN, SET_NAME, SET_USER } = authSlice.actions;
 
-export const selectIsLoggedIn = (state) => state.auth.isLoggedIn
-export const selectName = (state) => state.auth.name
-export const selectUser = (state) => state.auth.user
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
+export const selectName = (state) => state.auth.name;
+export const selectUser = (state) => state.auth.user;
 
 export default authSlice.reducer;
